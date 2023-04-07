b83cf1da5b1875a0475d9cca05b428a2fbe63d52
joshua <wanhoyinjoshua@yahoo.comgit config --global user.name wanhoyinjoshuagit config --global user.name wanhoyinjoshua>
2023-04-07 10:18:28 +1000

    hi

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
-/** @type {import('next').NextConfig} */
+
+
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development',
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com',"s.gravatar.com","anythinpocketimage.s3.ap-southeast-2.amazonaws.com"],
+  },
+};
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  
+  skipWaiting: true,
+  register: true,
+});
+
+module.exports = withPWA(nextConfig);
