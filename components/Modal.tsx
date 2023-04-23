de70a7ffd19c56fda825fa2f86fb2d63ea3f491d
joshua <wanhoyinjoshua@yahoo.comgit config --global user.name wanhoyinjoshuagit config --global user.name wanhoyinjoshua>
2023-04-24 05:38:59 +1000

    hi

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -48,25 +48,25 @@ const Modal = ({ showModal, setShowModal }) => {
     <>
       {showModal ? (
         <div className="fixed z-10 inset-0 overflow-y-auto">
-          <div className="flex items-center justify-center min-h-screen px-4 pt-6 pb-20 text-center sm:block sm:p-0">
+          <div className="flex w-full items-center justify-center min-h-screen px-4 pt-6 pb-20 text-center sm:block sm:p-0">
             <div className="fixed inset-0 transition-opacity" aria-hidden="true">
               <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
             </div>
             <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
             <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              <div className="bg-white px-4 pt-5 pb-4  sm:p-1 sm:pb-4">
                 <div className="flex flex-col sm:flex-col " >
                   <div className="self-start flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10" onClick={() => setShowModal(false)}>
                     <svg className="h-6 w-6 text-white-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
                     </svg>
                   </div>
-                  <div className="w-full mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                  <div className="w-full mt-3  text-center sm:mt-0 sm:ml-4 sm:text-left">
                     <h3 className="text-lg leading-6 font-medium text-gray-900"><input value={subject} placeholder='Enter your name here' onChange={(e) => setSubject(e.target.value)} /></h3>
                     <div className="mt-2">
                       <p className="text-sm text-gray-500">
-                      <div className="w-full">
-                                <ul className="border rounded-md divide-y divide-gray-200">
+                      <div className="w-full ">
+                                <ul className="h-1/2 border rounded-md divide-y divide-gray-200">
                                     {items.map((item) => (
                                     <li
                                         key={item}
