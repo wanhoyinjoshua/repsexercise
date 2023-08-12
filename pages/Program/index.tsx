72db1ed843c7242e9a3cddca974dedb1d5b5af94
joshua <wanhoyinjoshua@yahoo.comgit config --global user.name wanhoyinjoshuagit config --global user.name wanhoyinjoshua>
2023-08-12 18:57:18 +1000

    hi

--- pages/Program/index.tsx
+++ pages/Program/index.tsx
@@ -11,6 +11,7 @@ const Index = () => {
   const [data, setData] = useState(null);
   const [activevideo,setVideo]=useState(null)
 const [goalloading,setGoalloading]=useState(false)
+const [nomatch,setnomatch]=useState(false)
 
 
   const [searchValue, setSearchValue] = useState('');
@@ -70,6 +71,7 @@ setVideo(link)
     const data= await helper(searchValue)
     console.log(data)
     setData(data);
+
     setVideo(data[0]["videolink"])
     setSubmittedGoal(searchValue)
     setGoalloading(false)
@@ -94,7 +96,7 @@ setVideo(link)
     <div className='h-[90vh]'>
         <section className='mb-1.5'>
         <div
-  className="flex items-center justify-between gap-4 bg-rose-800 px-4 py-3 text-white"
+  className="flex items-center justify-between gap-4 px-4 py-3 text-black"
 >
   <p className="text-sm font-medium">
     This page allows you to find the right exercise videos for you based on your goals. Type your goal in the box below and hit search. Unsure what your goal is? Explore our example goals with the buttons below!
@@ -104,109 +106,10 @@ setVideo(link)
 
 </div>
         </section>
-        <button
-        onClick={()=>{setSearchValue("I want to be able better at walking outdoors so I can walk my dog, the exercises should be quite challenging. ")}}
-  className="inline-flex items-center justify-center rounded-full bg-emerald-100 px-2.5 py-0.5 text-emerald-700 hover:bg-emerald-200 hover:text-emerald-800"
->
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke-width="1.5"
-    stroke="currentColor"
-    className="-ms-1 me-1.5 h-4 w-4"
-  >
-    <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-    />
-  </svg>
-
-  <p className="whitespace-nowrap text-sm">Walking my dog</p>
-</button>
 
 
-<button
-  className="inline-flex items-center justify-center rounded-full bg-amber-100 px-2.5 py-0.5 text-amber-700 hover:bg-amber-200 hover:text-amber-800"
-  onClick={()=>{setSearchValue("I want to be better at opening the water bottle cap with my hands and reaching for the bottle by myself.")}}
->
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke-width="1.5"
-    stroke="currentColor"
-    className="-ms-1 me-1.5 h-4 w-4"
-  >
-    <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-    />
-  </svg>
-
-  <p className="whitespace-nowrap text-sm">Open the bottle cap by myself</p>
-</button>
-
-<button
-  className="inline-flex items-center justify-center rounded-full bg-emerald-100 px-2.5 py-0.5 text-emerald-700 hover:bg-emerald-200 hover:text-emerald-800"
-  onClick={()=>{setSearchValue("I want to be better at moving my shoulder to reach back to tie my hair and be better with my fingers")}}
->
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke-width="1.5"
-    stroke="currentColor"
-    className="-ms-1 me-1.5 h-4 w-4"
-  >
-    <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-    />
-  </svg>
-
-  <p className="whitespace-nowrap text-sm">Tie my hair up</p>
-</button>
-  <div className="mb-3">
-  <div className="relative mb-4 flex w-full flex-wrap items-stretch">
-    <input
-      type="search"
-      className="relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
-      placeholder="Type in your goal! Don't know what is on your mind? Click the buttons above to explore with our example goals!"
-      aria-label="Search"
-      aria-describedby="button-addon1" 
-      value={searchValue}
-    onChange={handleSearchChange}/>
-
-    
-    <button
-      className="relative z-2 flex items-center rounded-r bg-black px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
-      type="button"
-      id="button-addon1"
-      onClick={submit}
-      data-te-ripple-init
-      data-te-ripple-color="light">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="h-5 w-5">
-        <path
-          fill-rule="evenodd"
-          d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-          clip-rule="evenodd" />
-      </svg>
-    </button>
-  </div>
-</div>
-
-        <div className="h-full grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8">
-  <div className="h-full rounded-lg bg-gray-100">
-  <ul>
-  <div
+        <section>
+        <div
   role="alert"
   className="rounded-xl border border-gray-100 bg-white p-4 shadow-xl"
 >
@@ -248,24 +151,129 @@ setVideo(link)
       </svg>
     </span>}
     
-{goalloading?<div className="flex-1">
+{goalloading?
+<div className="flex-1">
       <strong className="block font-medium text-gray-900"> Finding exercises based on your goal </strong>
 
       <p className="mt-1 text-sm text-gray-700">
+        Finding exercises for<strong>{submittedgoal}</strong> <br></br>
         Almost there...
       </p>
-    </div>:<div className="flex-1">
-      <strong className="block font-medium text-gray-900"> Goals </strong>
+    </div>:
+    <div className="flex-1">
+     
 
-      <p className="mt-1 text-sm text-gray-700">
-        {submittedgoal}
-      </p>
+      <strong className="block font-medium text-gray-900"> Search our video library using your goal! </strong>
+
+      <div className="mt-1 text-sm text-gray-700">
+      <div>
+        <button
+          onClick={()=>{setSearchValue("I want to be able better at walking outdoors so I can walk my dog, the exercises should be quite challenging. ")}}
+    className="inline-flex items-center justify-center rounded-full bg-emerald-100 px-2.5 py-0.5 text-emerald-700 hover:bg-emerald-200 hover:text-emerald-800"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      className="-ms-1 me-1.5 h-4 w-4"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+
+    <div className="whitespace-nowrap text-sm">Walking my dog</div>
+        </button>
+
+
+        <button
+          className="inline-flex items-center justify-center rounded-full bg-amber-100 px-2.5 py-0.5 text-amber-700 hover:bg-amber-200 hover:text-amber-800"
+          onClick={()=>{setSearchValue("I want to be better at opening the water bottle cap with my hands and reaching for the bottle by myself.")}}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="-ms-1 me-1.5 h-4 w-4"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+
+          <div className="whitespace-nowrap text-sm">Open the bottle cap by myself</div>
+        </button>
+
+        <button
+          className="inline-flex items-center justify-center rounded-full bg-emerald-100 px-2.5 py-0.5 text-emerald-700 hover:bg-emerald-200 hover:text-emerald-800"
+          onClick={()=>{setSearchValue("I want to be better at moving my shoulder to reach back to tie my hair and be better with my fingers")}}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="-ms-1 me-1.5 h-4 w-4"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+
+          <div className="whitespace-nowrap text-sm">Tie my hair up</div>
+        </button>
+          <div className="mb-3">
+          <div className="mb-3">
+  <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+    <input
+      type="search"
+      className="relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+      placeholder="Search"
+      aria-label="Search"
+      aria-describedby="button-addon3" 
+      value={searchValue}
+      onChange={handleSearchChange}/>
+
+    <button
+      className="relative z-[2] rounded-r border-2 border-primary px-6 py-2 text-xs font-medium uppercase text-primary transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
+      type="button"
+      id="button-addon3"
+      onClick={submit}
+      data-te-ripple-init>
+      Search
+    </button>
+  </div>
+</div>
+          
+        </div>
+
+      </div>
+      </div>
     </div>}
     
 
     
   </div>
 </div>
+
+
+        </section>
+ 
+{data&&goalloading==false?  <div className="h-full grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8">
+  <div className="h-full rounded-lg bg-gray-100">
+  <ul>
+
     
 
     {data&&data.map((exercise)=>(
@@ -310,43 +318,38 @@ setVideo(link)
 </ul>
   </div>
   <div className="h-full rounded-lg bg-gray-100 lg:col-span-2">
-    {activevideo==null?<div></div>: <section>
-<div className='h-fit rounded-lg bg-gray-200 lg:col-span-2 bg-black'>
-        {goalloading?<Loading></Loading>:<ReactPlayer 
-      width='100%'
-      light={true}
-      
-      
-      controls={true} url={activevideo} 
-     
+    {activevideo==null?<div></div>: 
+    <section>
+      <div className='h-fit rounded-lg bg-gray-200 lg:col-span-2 bg-black'>
+          {goalloading?<Loading></Loading>:<ReactPlayer 
+        width='100%'
+        light={true}
         
-      />}
         
+        controls={true} url={activevideo} 
+      
+          
+        />}
+          
 
 
-    </div>
-    <div className='h-1/6 rounded-lg bg-gray-300 lg:col-span-2'>
-   
-
-<div className='w-full flex justify-end'>
-<a
-  className="inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
-  href=""
->
-  Next
-</a>
-
-</div>
+      </div>
+      <div className='h-1/6 rounded-lg bg-gray-300 lg:col-span-2'>
+    
 
 
-       
-    </div>
 
-</section>}
+
+        
+      </div>
+
+    </section>
+    }
 
  
   </div>
-</div>
+</div>:<div></div>}
+      
 
     </div>
    
