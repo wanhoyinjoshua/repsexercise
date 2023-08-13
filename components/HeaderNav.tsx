982d582d36a34958086f09fee7bd60dd509c5c8e
joshua <wanhoyinjoshua@yahoo.comgit config --global user.name wanhoyinjoshuagit config --global user.name wanhoyinjoshua>
2023-08-13 10:47:01 +1000

    hi

--- components/HeaderNav.tsx
+++ components/HeaderNav.tsx
@@ -28,7 +28,7 @@ const HeaderNav = () => {
         
 <nav className=" h-[10vh] bg-white border-gray-200 dark:bg-gray-900 ">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-  <a href="https://flowbite.com/" className="flex items-center">
+  <a href="/" className="flex items-center">
       <img src="/icon-48x48.png" className="h-8 mr-3" alt="Flowbite Logo" />
       <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">REPS</span>
   </a>
