e35a88bf1cb5b0f9acbaa3993d05c7cbf142926b
joshua <wanhoyinjoshua@yahoo.comgit config --global user.name wanhoyinjoshuagit config --global user.name wanhoyinjoshua>
2024-08-25 19:11:19 +1000

    hi

--- app/VideoBooklet/page.tsx
+++ app/VideoBooklet/page.tsx
@@ -4,6 +4,32 @@ import Player from '@vimeo/player'
 import { useRef } from 'react'
 const Page = () => {
     const iframeRef = useRef(null);
+    function loadvideo(){
+        var player= new Player(iframeRef.current)  
+        player.loadVideo(285448922).then(function(id) {
+            // the video successfully loaded
+            playvideo()
+        }).catch(function(error) {
+            switch (error.name) {
+                case 'TypeError':
+                    // the id was not a number
+                    break;
+        
+                case 'PasswordError':
+                    // the video is password-protected and the viewer needs to enter the
+                    // password first
+                    break;
+        
+                case 'PrivacyError':
+                    // the video is password-protected or private
+                    break;
+        
+                default:
+                    // some other error occurred
+                    break;
+            }
+        });
+    }
     function playvideo(){
 
       var player= new Player(iframeRef.current)  
@@ -32,7 +58,7 @@ const Page = () => {
   return (
     <div className="mx-auto max-w-7xl px-6 lg:px-8">
         VideoBooklet
-        <button onClick={playvideo}>Play</button>
+        <button onClick={loadvideo}>Play</button>
         <iframe  ref={iframeRef} src="https://player.vimeo.com/video/285448922"  allow="autoplay; fullscreen; picture-in-picture; clipboard-write" title="Exercise 1 - The Shoulder PUSH"></iframe>
     
     </div>
