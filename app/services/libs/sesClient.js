1279a6955c567a138e08b973164e9cc6bc9ea612
joshua <wanhoyinjoshua@yahoo.comgit config --global user.name wanhoyinjoshuagit config --global user.name wanhoyinjoshua>
2024-09-02 07:00:45 +1000

    hi

--- app/services/libs/sesClient.js
+++ app/services/libs/sesClient.js
@@ -4,8 +4,8 @@ const REGION = "ap-southeast-2";
 
 // Create SES service object.
 const sesClient = new SESClient({ region: REGION , credentials: {
-    accessKeyId: process.env.AWSKEY,
-    secretAccessKey:process.env.AWSSECRET
+    accessKeyId: process.env.NEXT_PUBLIC_AWSKEY,
+    secretAccessKey:process.env.NEXT_PUBLIC_AWSSECRET
   }});
 export { sesClient };
 // snippet-end:[ses.JavaScript.createclientv3]
